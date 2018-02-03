2b020be01b30c3da364c4096f990e115914ee349
Attila Keri <attila.keri@icloud.com>
2018-02-03 16:12:54 +0000

    updated .gitignore, add fetch to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      url:'https://api.coinmarketcap.com/v1/ticker/',
+      allTheCoins: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData(this.state.url)
+  }
+
+  fetchData(url) {
+    fetch(url)
+      .then(data => data.json())
+      .then(json => this.setState({allTheCoins: json}))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div>
+        <h1>Hello!</h1>
+      </div>       
+    )
   }
 }
 
